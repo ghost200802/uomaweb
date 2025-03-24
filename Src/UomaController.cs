a6e46f8cceb7cf690a5efd188dcb4a7d6ee78ffc
Ghost <ghost200802@163.com>
2025-03-24 12:35:41 +0800

    static UomaController

--- Src/UomaController.cs
+++ Src/UomaController.cs
@@ -4,44 +4,38 @@ using UomaWeb.Models;
 
 namespace UomaWeb
 {
-    public class UomaController
+    public static class UomaController
     {
-        private readonly GameHelper _gameHelper;
-        protected string _gameName;
+        private static readonly GameHelper _gameHelper = new GameHelper();
 
-        public string GameName => _gameName;
+        public static string GameName {get; set;}
 
-        public UomaController()
-        {
-            _gameHelper = new GameHelper();
-        }
-
-        public async Task<int> GetCurrentLevelAsync()
+        public static async Task<int> GetCurrentLevelAsync()
         {
             return await _gameHelper.GetPlayerCurrLevelAsync(GameName);
         }
 
-        public async Task<(int successCode, int currLevel, int currencyNum)> CompleteLevelAsync(int gameLevel, int star)
+        public static async Task<(int successCode, int currLevel, int currencyNum)> CompleteLevelAsync(int gameLevel, int star)
         {
             return await _gameHelper.CompleteLevelAsync(GameName, gameLevel, star);
         }
 
-        public async Task<int> GetVirtualCurrencyAsync()
+        public static async Task<int> GetVirtualCurrencyAsync()
         {
             return await _gameHelper.GetVirtualCurrencyAsync();
         }
 
-        public async Task<(int successCode, int currencyNum, int itemNum)> UseGameItemAsync(string itemName)
+        public static async Task<(int successCode, int currencyNum, int itemNum)> UseGameItemAsync(string itemName)
         {
             return await _gameHelper.UseGameItemAsync(GameName, itemName);
         }
 
-        public async Task<(int successCode, int currencyNum, int itemNum)> BuyGameItemAsync(string itemName, int num)
+        public static async Task<(int successCode, int currencyNum, int itemNum)> BuyGameItemAsync(string itemName, int num)
         {
             return await _gameHelper.BuyGameItemAsync(GameName, itemName, num);
         }
 
-        public async Task<Dictionary<string, int>> GetGameItemNumAsync()
+        public static async Task<Dictionary<string, int>> GetGameItemNumAsync()
         {
             return await _gameHelper.GetGameItemNumAsync(GameName);
         }
