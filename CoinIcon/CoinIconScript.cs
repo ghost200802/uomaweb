38dcac898a44ebe258942c87bc6e365fdca31f31
Ghost <ghost200802@163.com>
2025-07-28 19:39:50 +0800

    add CoinIcon

---
+++ CoinIcon/CoinIconScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UomaWeb;
+
+public class CoinIconScript : MonoBehaviour
+{
+    public Text txtCoin;
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtCoin.text = $"{UomaDataManager.GetVirtualCurrency()}";
+    }
+
+}
