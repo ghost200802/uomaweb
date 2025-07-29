3df519b69bd3e299618a1df3c2e20bc8b23a1c9c
Ghost <ghost200802@163.com>
2025-07-29 20:12:57 +0800

    modify

--- Src/Data/UomaGameConfig.cs
+++ Src/Data/UomaGameConfig.cs
@@ -11,39 +11,52 @@ namespace UomaWeb
             {
                 Games = new Dictionary<string, GameAction>
                 {
-                    { "CandySweet", new GameAction
+                    {
+                        "CandySweet", new GameAction
                         {
                             id = "1904083505756282880",
                             name = "消消乐",
                             description = "一款经典的三消类游戏"
                         }
                     },
-                    { "ScrewRemoval", new GameAction
+                    {
+                        "ScrewRemoval", new GameAction
                         {
                             id = "1904079869357629440",
                             name = "钉钉子",
                             description = "考验技巧的益智游戏"
                         }
                     },
-                    { "WaterSort", new GameAction
+                    {
+                        "WaterSort", new GameAction
                         {
                             id = "1904084067851739136",
                             name = "倒水",
                             description = "考验技巧的益智游戏"
                         }
                     },
-                    { "KnifeHit", new GameAction
+                    {
+                        "KnifeHit", new GameAction
                         {
                             id = "1904086016353411072",
                             name = "飞刀",
                             description = "考验技巧的益智游戏"
                         }
                     },
-                    { "TilesMatching", new GameAction
+                    {
+                        "TilesMatching", new GameAction
                         {
                             id = "1944714241605410816",
                             name = "方块消除",
                             description = "方块消除的游戏"
+                        }
+                    },
+                    {
+                        "HelpTheDuck", new GameAction
+                        {
+                            id = "1949785169808891904",
+                            name = "救救鸭子",
+                            description = "益智解谜游戏"
                         }
                     }
                 }
